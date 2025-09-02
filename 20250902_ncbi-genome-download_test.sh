b443c6ea752f1568570da9535668c6ccaa1d2d92
yaoyunying-max <77580428+yaoyunying-max@users.noreply.github.com>
2025-09-02 16:02:00 +0800

    Create 20250902_ncbi-genome-download_test.sh

---
+++ 20250902_ncbi-genome-download_test.sh
@@ -0,0 +1,9 @@
+source /coldata/yanjiuyuan/yaoyy/New_home/.bashrc
+
+pip install ncbi-genome-download
+
+pip install --target=/coldata/yanjiuyuan/yaoyy/New_home/miniconda3/pkgs/ ncbi-genome-download
+
+
+## 
+conda info ## 查看conda环境信息
